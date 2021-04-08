aa24e93b3b40d18fe297373728ef4d3d04df8e56
jeonghar1m <harim97714@mail.hongik.ac.kr>
2021-04-09 04:00:39 +0900

    create

---
+++ HW1/ChildView.cpp
@@ -0,0 +1,150 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "HW1.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_ERASEBKGND()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	CDC memDC;
+	memDC.CreateCompatibleDC(&dc);
+
+	CBitmap bitmap;
+	bitmap.CreateCompatibleBitmap(&dc, rect.Width(), rect.Height());
+	memDC.SelectObject(&bitmap);
+
+	memDC.Rectangle(-10, -10, rect.Width() + 10, rect.Height() + 10);
+
+	if (m_LButtonDown)
+	{
+		CBrush brush(RGB(255, 0, 0));
+		memDC.SelectObject(&brush);
+		memDC.Rectangle(m_StartPoint.x, m_StartPoint.y, m_EndPoint.x, m_EndPoint.y);
+	}
+
+	if (m_RButtonDown)
+	{
+		CBrush brush(RGB(0, 0, 255));
+		memDC.SelectObject(&brush);
+		memDC.Ellipse(m_StartPoint.x, m_StartPoint.y, m_EndPoint.x, m_EndPoint.y);
+	}
+
+	dc.BitBlt(0, 0, rect.Width(), rect.Height(), &memDC, 0, 0, SRCCOPY);
+	
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_LButtonDown = true;
+	m_StartPoint = point;
+
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_LButtonDown = false;
+	m_StartPoint = point;
+
+	CWnd::OnLButtonUp(nFlags, point);
+}
+
+
+void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_LButtonDown = true;
+	m_StartPoint = point;
+
+	CWnd::OnRButtonDown(nFlags, point);
+}
+
+
+void CChildView::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_LButtonDown = false;
+	m_StartPoint = point;
+
+	CWnd::OnRButtonUp(nFlags, point);
+}
+
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	if (m_LButtonDown || m_RButtonDown)
+	{
+		m_EndPoint = point;
+		Invalidate();
+	}
+
+	CWnd::OnMouseMove(nFlags, point);
+}
+
+
+BOOL CChildView::OnEraseBkgnd(CDC* pDC)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	return true;
+}
