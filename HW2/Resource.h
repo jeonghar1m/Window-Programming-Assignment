3906219f6fd33c626f5f5c9309f185f8f9aaea51
jeonghar1m <harim97714@mail.hongik.ac.kr>
2021-05-20 02:43:57 +0900

    update toolbar

--- HW2/Resource.h
+++ HW2/Resource.h
@@ -10,13 +10,15 @@
 #define ID_COLOR_GREEN                  32772
 #define ID_COLOR_BLUE                   32773
 #define ID_COLOR_BLACK                  32774
+#define ID_BUTTON32776                  32776
+#define ID_BUTTON32778                  32778
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
